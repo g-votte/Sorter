4617342d3294445c52c9107c9b43b074eea53bab
sano <s.g.votte@gmail.com>
2011-09-21 16:39:57 +0900

    third commit

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,63 @@
-# implement this
+use strict;
+use warnings;
+
+package Sorter;
+
+sub new {
+ my $class = shift();
+
+ my $self = {
+  values => []
+ };
+
+ return bless $self, $class;
+}
+
+sub set_values {
+ my $self = shift();
+
+ foreach my $value (@_) {
+  push @{$self->{values}}, $value;
+ }
+}
+
+sub get_values {
+ my $self = shift();
+
+ return @{$self->{values}};
+}
+
+sub sort {
+ sub quick_sort {
+  my @values = @{shift()};
+
+  if(@values<=0){
+   return @values;
+  }
+
+  my $pivot = $values[0];
+  my @firsthalf;
+  my @secondhalf;
+
+  foreach (@values[1..$#values]) {
+   if ($_<=$pivot) {
+    push @firsthalf, $_;
+   }else{
+    push @secondhalf, $_;
+   }
+  }
+
+  @firsthalf = quick_sort(\@firsthalf);
+  @secondhalf = quick_sort(\@secondhalf);
+
+  push(@firsthalf, $pivot);
+  push(@firsthalf, @secondhalf);
+
+  return @firsthalf;
+ }
+
+ my $self = shift;
+ @{$self->{values}} = quick_sort(\@{$self->{values}});
+}
+
+1;
